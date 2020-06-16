48f75457f6982621bd90598890f0ff734ecfaf6e
Qi <784359442@qq.com>
2020-06-15 21:22:05 -0700

    Update project

--- models.py
+++ models.py
@@ -3,6 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # this
+
 database_path = os.environ['DATABASE_URL']
 # database_path = 'postgresql://postgres:password@localhost:5432/heroku'
 
@@ -12,6 +22,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -24,19 +36,21 @@ def setup_db(app, database_path=database_path):
 Person
 Have title and release year
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+class Person(db.Model):
+    __tablename__ = 'People'
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    catchphrase = Column(String)
+
+    def __init__(self, name, catchphrase=""):
+        self.name = name
+        self.catchphrase = catchphrase
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'catchphrase': self.catchphrase}
